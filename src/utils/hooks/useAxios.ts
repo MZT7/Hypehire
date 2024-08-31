37b2cc5649ba96eff36ad260b9e349d6412d62d2
MZT7 <40639877+MZT7@users.noreply.github.com>
2024-08-31 11:01:37 +0100

    Update useAxios.ts

--- src/utils/hooks/useAxios.ts
+++ src/utils/hooks/useAxios.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 const useAxios = () => {
   const axiosInstance = axios.create({
     baseURL: "https://hyper.mezudev.site/public/api",
+    withCredentials: true,
   });
 
   return axiosInstance;
